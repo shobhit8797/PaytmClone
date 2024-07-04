88e219ea862b9f15bfec40c6dc527d31d5da9927
shobhit <shobhit.goyal@carefi.in>
2024-07-04 23:42:00 +0530

    Base frontend setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Signup} from "./pages/Signup";
 function App() {
-
-  return (
-    <div>
-        Hello world
-    </div>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/signup" element={<Signup />} />
+                    {/* <Route path="/signin" element={<Signin />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/send" element={<SendMoney />} /> */}
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+export const Signup = () => {
+    return (
+        <div className="bg-slate-300 h-screen justify-center flex">
+            <div className="flex flex-col justify-center">
+                <div className="bg-white">SignUp</div>
+            </div>
+        </div>
+    );
+};
